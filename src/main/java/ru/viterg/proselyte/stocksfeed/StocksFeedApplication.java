4ecba44eb4117f784ddf17cc1521c736e0b04f29
Viterg <viterg@bk.ru>
2023-10-05 22:19:09 +0400

    client moved to stocks-observer

--- src/main/java/ru/viterg/proselyte/stocksfeed/StocksFeedApplication.java
+++ src/main/java/ru/viterg/proselyte/stocksfeed/StocksFeedApplication.java
@@ -1,20 +1,11 @@
 package ru.viterg.proselyte.stocksfeed;
 
-import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import java.time.Duration;
-
-import static io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 @EnableCaching
 @SpringBootApplication
@@ -27,21 +18,5 @@ public class StocksFeedApplication {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public WebClient webClient(HttpClient httpClient) {
-        return WebClient.builder()
-                .baseUrl("https://financialmodelingprep.com/api/v3")
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
-    }
-
-    @Bean
-    public HttpClient httpClient() {
-        return HttpClient.create()
-                .option(CONNECT_TIMEOUT_MILLIS, 2000)
-                .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(1000, MILLISECONDS)))
-                .responseTimeout(Duration.ofMillis(1000));
     }
 }
